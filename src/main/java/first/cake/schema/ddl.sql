b482935f65752d05178bbe467d063c2cf8842080
shin <jakie5803@gmail.com>
2023-07-15 14:44:41 +0900

    create chatting_room query add

---
+++ src/main/java/first/cake/schema/ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `cake`.`cake_chatting_room` (
+             `room_id` VARCHAR(28) NULL,
+             `store_name` VARCHAR(20) NULL,
+             `customer_id` VARCHAR(30) NULL,
+             `user_count` INT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
