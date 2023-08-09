ab2c7d4f484a13a2840f2f8c2fbf19aec26e2e64
jhshin <jakie580@gmail.com>
2023-08-09 13:45:44 +0900

    코드수정21

--- src/main/java/first/cake/config/AmazonS3Config.java
+++ src/main/java/first/cake/config/AmazonS3Config.java
@@ -2,20 +2,22 @@ package first.cake.config;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Configuration
 public class AmazonS3Config {
 
-
-    @Value("${cloud.aws.s3.region}")
+    @Value("${cloud.aws.s3.region:not-used}")
     private String region;
 
     @Bean
     public AmazonS3 amazonS3() {
+        log.info(region);
         return AmazonS3Client.builder()
                 .withRegion(region)
                 .build();
