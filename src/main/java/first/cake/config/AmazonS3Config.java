3997cfb74ebc487d1bd92a1d2cac6bbe9fd3e37b
jhshin <jakie580@gmail.com>
2023-08-09 13:18:36 +0900

    코드수정17

--- src/main/java/first/cake/config/AmazonS3Config.java
+++ src/main/java/first/cake/config/AmazonS3Config.java
@@ -5,13 +5,12 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Configuration
 public class AmazonS3Config {
 
-    @Lazy
+
     @Value("${cloud.aws.s3.region}")
     private String region;
 
