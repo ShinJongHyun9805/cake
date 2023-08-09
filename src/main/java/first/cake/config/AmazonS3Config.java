41dd0152296feca52319efa911d52456d0c8a97c
jhshin <jakie580@gmail.com>
2023-08-09 17:22:43 +0900

    코드수정29

--- src/main/java/first/cake/config/AmazonS3Config.java
+++ src/main/java/first/cake/config/AmazonS3Config.java
@@ -1,25 +1,36 @@
 package first.cake.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Configuration
 public class AmazonS3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Value("${cloud.aws.s3.region}")
     private String region;
 
     @Bean
     public AmazonS3 amazonS3() {
-        log.info(region);
-        return AmazonS3Client.builder()
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
                 .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
     }
 }
